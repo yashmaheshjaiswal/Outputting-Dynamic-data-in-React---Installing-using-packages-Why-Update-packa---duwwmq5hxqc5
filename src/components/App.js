d8da82dac8ce5326064bb42190c55a59ded96ba4
yashmaheshjaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-01-17 14:07:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here
+  //code here
+  const [input, setInput] = useState('');
+  const [blank, setBlank] = useState('____');
+  
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={(e) => setInput(e.target.value)}></input>
+      <button id='button' onClick={() => setBlank(input)}>Click</button>
+      <p id='text'> Hello my name is {blank} and I study at Newton School</p>
     </div>
   )
 }
